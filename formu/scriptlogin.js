a7e597e60f8b761db0b194813957a42c9a82dfba
King e$tx <estonjr2@hotmai.com>
2020-05-08 23:15:39 -0300

    commit

---
+++ formu/scriptlogin.js
@@ -0,0 +1,9 @@
+function mostrar() {
+    document.getElementById('oculto').style.display = 'block';
+}
+
+function ocultar() {
+    document.getElementById('oculto').style.display = 'none';
+}
+
+console.log("oi")
